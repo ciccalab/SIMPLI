3179e75ec886bb66c8140d27321d4ab116d4a062
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2020-07-06 18:05:51 +0100

    Moved plotting functions to Plot_Functions.R to make them reusable.

---
+++ scripts/Plot_Functions.R
@@ -0,0 +1,181 @@
+library(data.table)
+library(ggplot2)
+library(ggrepel)
+
+######## Heatmaps #######
+heatmapper <- function(plot_dataset, res_column, cols, high_color, mid_color, low_color)
+{
+  plot_data <- copy(plot_dataset)
+  setnames(plot_data, res_column, "cluster")
+  plot_data <- plot_data[, lapply(.SD, mean), by = cluster, .SDcols = cols]
+  plot_data[, cluster := as.character(cluster)]  
+  plot_data <- melt(plot_data, measure.vars = cols, id.vars = "cluster")
+  setnames(plot_data, c("variable", "value"), c("gene", "expression"))
+  plot_data[, scaled_expression := (expression - min(expression)) / (max(expression) - min(expression)), by = gene]
+  x_labels <- as.character(sort(as.numeric(unique(plot_data$cluster))))
+  ggplot(data = plot_data) +
+    geom_tile(mapping = aes(x = cluster, y = gene, fill = scaled_expression)) +
+    geom_text(mapping = aes(x = cluster, y = gene, label = formatC(expression, digits = 2, format = "g")),
+              color = "black", size = 5) + 
+    scale_x_discrete(limits = x_labels, labels = x_labels, name = element_blank()) +
+    scale_fill_gradient2(low = low_color, high = high_color, mid = mid_color, midpoint = 0.5, name = element_blank()) +
+    scale_y_discrete(name = element_blank(), limits = rev(markers)) +
+    theme_classic() + theme(text = element_text(size = 20), plot.margin = margin(t = 0, r = 0, b = 0,l = 0,
+    unit = "pt"), plot.title = element_text(hjust = 0.5), axis.text.x = element_text(size = 15))
+}
+
+####### Scatterplots #######
+label_dot_plotter <- function(data, x_name, y_name, marker_name)
+{
+  plot_data <- copy(data)
+  data[[marker_name]] <- as.character(data[[marker_name]])	
+  my_plot <- ggplot(data) +
+    geom_point(mapping = aes_string(x = x_name, y = y_name, color = marker_name)) +
+    labs(x = x_name, y = y_name) +
+    theme_bw(base_size = 12, base_family = "sans") +
+    theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank(),
+          legend.position = "right", panel.border = element_blank(), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), axis.line = element_line(colour = "black", size = 0.75),
+          strip.background = element_rect(colour = NA, fill = NA))
+  return(my_plot)
+}
+
+gradient_dot_plotter <- function(data, x_name, y_name, marker, highcol, lowcol)
+{
+  plot_data <- copy(data)
+  setnames(plot_data, c(x_name, y_name, marker), c("x_name", "y_name", "marker"))
+  plot_data[, marker := marker / max(marker)]
+  my_plot <- ggplot(plot_data) +
+    geom_point(mapping = aes(x = x_name, y = y_name, color = marker)) +
+    scale_color_gradient(low = lowcol, high = highcol) +
+    theme_bw(base_size = 12, base_family = "sans") +
+    labs(x = x_name, y = y_name) +
+    theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank(),
+          legend.position = "right", panel.border = element_blank(), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), axis.line = element_line(colour = "black", size = 0.75),
+          strip.background = element_rect(colour = NA, fill = NA))
+  return(my_plot)
+}
+
+####### Boxplots #######
+threshold_e = 0.01
+round_format_2 <- function(number, threshold = threshold_e)
+{
+	if (is.nan(number)) return("Nan")
+	if (number == 0) return("0")
+	if (number >= threshold) return(as.character(round(number, 2)))
+	return(paste0(round(as.numeric(sub("e.*", "", formatC(number, format = "e"))), 2), "×10^", sub("^[^-]*", "",
+		formatC(number, format = "e"))))
+}
+
+boxplotter <- function(data, yaxis_variable_name, yaxis_title, group_variable_name, sample_column_name, bp_title,
+  axis_stroke = 0.75) 
+{
+	local_data <- as.data.frame(copy(data))
+	list_return <- list()
+	# Make sure no hyphen is in the name of the group levels:
+	local_data[, group_variable_name] <- as.character(local_data[, group_variable_name])
+	local_data[, group_variable_name] <- gsub(pattern = "-", replacement = " ", local_data[, group_variable_name])
+	# Coerce group variable into alphabetically ordered factor:
+	local_data[, group_variable_name] <- factor(local_data[, group_variable_name],
+	levels = as.character(sort(unique(local_data[, group_variable_name]))))
+	# Number of samples in each group:
+	uss <- unique(local_data[,c(sample_column_name, group_variable_name)])
+	sum_samples_g1 <- sum(uss[,group_variable_name] == levels(uss[,group_variable_name])[1])
+	sum_samples_g2 <- sum(uss[,group_variable_name] == levels(uss[,group_variable_name])[2])
+	# Two-tailed Wilcoxon test between the two groups:
+	g1 <- local_data[local_data[,group_variable_name] == levels(local_data[,group_variable_name])[1], yaxis_variable_name]
+	g2 <- local_data[local_data[,group_variable_name] == levels(local_data[,group_variable_name])[2], yaxis_variable_name]
+	wilcox_groups <- wilcox.test(g1, g2)
+	list_return[["pval"]] <- wilcox_groups$p.value
+	# Creating BP:
+	list_return[["bp"]] <- ggplot(data = local_data, aes_string(x = group_variable_name, y = yaxis_variable_name,
+		label = sample_column_name)) +
+	geom_boxplot(fill = NA, width = 0.35, outlier.shape = NA) +
+	geom_point(fill = "black") +
+	geom_text_repel(data = subset(local_data, get(group_variable_name) == levels(local_data[, group_variable_name])[1]),
+		nudge_x = 0.3, direction = "y", hjust = 0, size = 2) +
+	geom_text_repel(data = subset(local_data, get(group_variable_name) == levels(local_data[,group_variable_name])[2]),
+		nudge_x = 1.7, direction = "y", hjust = 1, size = 2) +
+	scale_x_discrete(labels = c(paste0(toupper(abbreviate(levels(local_data[,group_variable_name])[1])),
+		" (", sum_samples_g1, ")"), paste0(toupper(abbreviate(levels(local_data[, group_variable_name])[2])),
+		" (", sum_samples_g2, ")")), name = element_blank()) +  
+	scale_y_continuous(name = yaxis_title, limits = c(0, max(local_data[,yaxis_variable_name])),
+		breaks = seq(from = 0, to = round(max(local_data[,yaxis_variable_name])),
+		length.out = 10), labels = round(seq(from = 0, to = round(max(local_data[,yaxis_variable_name])), length.out = 10))) +
+	ggtitle(bp_title) + labs(subtitle = paste0("p = ", round_format_2(list_return[["pval"]]))) +
+	theme_bw(base_size = 8, base_family = "sans") +
+	theme(plot.title = element_text(hjust = 0.5), legend.title = element_blank(), legend.position = "none",
+			panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+			axis.line = element_line(colour = "black", size = axis_stroke), strip.background = element_rect(colour = NA,
+			fill = NA), plot.subtitle = element_text(size = 8, hjust = 0.5, vjust = 1))
+	return(list_return)
+}
+
+list_boxplotter <- function(data, sample_column, type_column, group_variable_name, y_column, axis_stroke = 0.75, correct = TRUE)
+{
+  types <- sort(unique(data[[type_column]]))
+  plot_dataset <- copy(data)[, .SD, .SDcols = c(sample_column, group_variable_name, type_column, y_column)]
+  setnames(plot_dataset, c(sample_column, group_variable_name, type_column, y_column),
+    c("sample_column", "x_column", "type_column", "y_column"))
+  plot_dataset <- unique(plot_dataset[, .(sample_column, x_column, y_column), keyby = type_column])
+  plots <- lapply(types, function(type)
+  {
+    boxplotter(plot_dataset[type_column == type], "y_column", "Cells %", group_variable_name = "x_column",
+      sample_column_name = "sample_column", bp_title = type, axis_stroke = axis_stroke)
+  })
+  if (correct)
+  {
+    FDRs <-  p.adjust(sapply(plots, `[[`, 1), method = "BH")
+    plots <- lapply(seq_along(plots), function(n){
+      pval <- plots[[n]][["pval"]]
+      plt <- plots[[n]][["bp"]]
+      FDR <- FDRs[[n]]
+      plt <- plt + labs(subtitle =  paste0("p = ", round_format_2(pval), "\nFDR = ", round_format_2(FDR))) +
+        theme(plot.subtitle = element_text(size = 8, hjust = 0.5, vjust = 1))
+      return(list(pvalue = pval, FDR_BH = FDR, Plot = plt))        
+    })
+  }
+  names(plots) <- types
+  return(plots)
+}
+
+####### Image Processing #######
+load_image <- function(filename)
+{
+	Img <- readImage(filename, "tiff", all = FALSE)
+	if (length(dim(Img)) == 3) {return(Img[,,1])}
+	return(Img)
+}
+
+color_imager <- function(pic_list, color_list)
+{
+	pics <- lapply(seq_along(pic_list), function(n){
+		color_ratio <- col2rgb(color_list[[n]]) / 255
+		rgbImage(pic_list[[n]] * color_ratio[[1]], pic_list[[n]] * color_ratio[[2]], pic_list[[n]] * color_ratio[[3]])
+	})
+	Reduce(`+`, pics)
+ }
+
+cell_overlayer <- function(picture, mask, cell_list, color_list)
+{
+	pics <- lapply(seq_along(cell_list), function(n){
+		cell_mask <- mask * (2^16 - 1)
+		pic <- Image(dim = c(dim(cell_mask),3), colormode = "Color")
+		cell_ns <- cell_list[[n]][, as.integer(sub(".+-", "", Cell_ID))]
+		cell_mask[which(!(cell_mask %in% cell_ns), arr.ind = T)] <- 0
+		paintObjects(cell_mask, pic, col = color_list[[n]], thick = T)
+	})
+	pic <- Reduce(`+`, pics)
+	return(pic + ifelse(any(picture) > 0, rgbImage(picture, picture, picture), 0))
+}
+
+################## PDF Output ##################
+pdf_w <- 8.27 / 2 # About 1/6th of an A4 (inches)
+pdf_h <- 11.69 / 3
+pdf_plotter <- function(filename, plot, width = pdf_w, height = pdf_h)
+{
+	pdf(filename, width = width, height = height, useDingbats = F)
+	print(plot)
+	dev.off()
+}
