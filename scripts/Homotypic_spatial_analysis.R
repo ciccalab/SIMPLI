c0b5961f5815abf8a76f486596e86d7946ee07e0
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2021-01-06 18:58:22 +0000

    Fixed bug by converting arguments 2 and 3 to numeric. Location_Center_Z removed. Added isseed column to output.

--- scripts/Homotypic_spatial_analysis.R
+++ scripts/Homotypic_spatial_analysis.R
@@ -4,8 +4,8 @@ library(fpc)
 
 arguments <- commandArgs(trailingOnly = TRUE)
 coordinates_file_name <- arguments[[1]]
-reachability_distance <- arguments[[2]]
-min_cells <- arguments[[3]]
+reachability_distance <- as.numeric(arguments[[2]])
+min_cells <- as.numeric(arguments[[3]])
 cell_type_column <- arguments[[4]]
 cell_type_to_cluster <- arguments[[5]]
 output_folder <- arguments[[6]]
@@ -20,11 +20,12 @@ samples <- unique(coordinates$Metadata_sample_name)
 ###### Apply dbscan for each sample and cell type of interest ######
 dbscan_output <- lapply(samples, function(sample){
     out_dbscan <- coordinates[cell_type == cell_type_to_cluster & Metadata_sample_name == sample, .(CellName,
-		Metadata_sample_name, Location_Center_X, Location_Center_Y, Location_Center_Z, cell_type)]
+		Metadata_sample_name, Location_Center_X, Location_Center_Y, cell_type)]
     if (nrow(out_dbscan) > 0) {
         fpc_coordinates <- out_dbscan[, .(Location_Center_X, Location_Center_Y)]
         dbscan_results_per_sample <- fpc::dbscan(data = fpc_coordinates, e = reachability_distance, MinPts = min_cells, method = "raw")
         out_dbscan$cluster <- dbscan_results_per_sample$cluster
+        out_dbscan$isseed <- ifelse(is.null(dbscan_results_per_sample$isseed), FALSE, dbscan_results_per_sample$isseed)
         return(out_dbscan)
 	}
 	return(NULL)
