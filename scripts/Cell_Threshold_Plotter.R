4843855ffc572d1f8a043eee041e3e42c376aff7
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2021-05-03 09:42:13 +0100

    Fix error in legend with many entries.

--- scripts/Cell_Threshold_Plotter.R
+++ scripts/Cell_Threshold_Plotter.R
@@ -64,18 +64,20 @@ cell_overlays <- lapply(populations, function(pop){
 names(cell_overlays) <- populations
 
 legend_tables <- lapply(populations, function(pop){
-  legend_table <- data.table(cell_type = names(Population_Colors[[pop]]), color = Population_Colors[[pop]])
-  legend_table[, X := (.I-1)%%round(sqrt(.N))*round(sqrt(.N)) + 1]
-  legend_table[, text_X := X + round(sqrt(.N)) - 0.5]
-  legend_table[, Y := -(0:round(sqrt(.N))), by = X]
-  legend_plot <- ggplot(data = legend_table) +
-    geom_tile(mapping = aes(x = X, y = Y, fill = cell_type), width = 0.5, height = 0.5, color = "black") +
-    geom_text(mapping = aes(x = text_X, y = Y, label = cell_type), hjust = "right", size = 3) +
-    scale_fill_manual(values = Population_Colors[[pop]], guide = "none") +
-    coord_equal() +
-    theme(axis.line=element_blank(), axis.text = element_blank(), axis.title = element_blank(),
-          axis.ticks = element_blank(), panel.background = element_blank(), panel.border = element_blank(),
-          panel.grid.major = element_blank(), panel.grid.minor = element_blank(), plot.background = element_blank())
+	legend_table <- data.table(cell_type = names(Population_Colors[[pop]]), color = Population_Colors[[pop]])
+	legend_table[, X := 2 + 2 * (.I %% ceiling(sqrt(.N)))]
+	legend_table[, text_X := X - 0.35]
+	legend_table[, Y := 1 + .N - (1:.N), by = X]
+	legend_plot <- ggplot(data = legend_table) +
+		geom_tile(mapping = aes(x = X, y = Y, fill = cell_type), width = 0.5, height = 0.5, color = "black") +
+		geom_text(mapping = aes(x = text_X, y = Y, label = cell_type), hjust = "right", size = 1) +
+		scale_fill_manual(values = Population_Colors[[pop]], guide = "none") +
+		xlim(0, max(legend_table$X + 1)) +	
+		ylim(0, max(legend_table$Y + 1)) +	
+		coord_equal() +
+		theme(axis.line=element_blank(), axis.text = element_blank(), axis.title = element_blank(),
+			axis.ticks = element_blank(), panel.background = element_blank(), panel.border = element_blank(),
+			panel.grid.major = element_blank(), panel.grid.minor = element_blank(), plot.background = element_blank())
 })
 names(legend_tables) <- populations
 
